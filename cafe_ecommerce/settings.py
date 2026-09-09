c482b5160ffed1c086ae35b649b0270093370c4d
adiishhh <mohamedadish20@gmail.com>
2026-09-10 00:07:34 +0530

    Category app created

--- cafe_ecommerce/settings.py
+++ cafe_ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'users',
+    'category',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
