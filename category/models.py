650f4d4e75cc46756559aa14972190e2469a1172
adiishhh <mohamedadish20@gmail.com>
2026-09-10 16:40:17 +0530

    minor category updation (admin)

--- category/models.py
+++ category/models.py
@@ -8,3 +8,6 @@ class Category(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
